a2b6f0e727875532729babf7134d3aa1bfbc139f
andreobriennz <andre@communica.co.nz>
2018-04-13 12:41:08 +1200

    Remove redundant code from main.js

--- src/main.js
+++ src/main.js
@@ -1,36 +1,9 @@
 import Vue from 'vue'
-// import VueRouter from 'vue-router'
-// import _ from 'lodash'
 
 import App from './App.vue'
-
-// Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
     render: h => h(App)
-    // consider removing evenrything below as its duplicated in App.vue and Layout.vue
-    // data: {
-    //     h1: 'Andre OBrien <strong>Web Developer</strong>',
-    //     mobileNav: false
-    // },
-    // computed: {
-    //     page: function () {
-    //       return this.$route.name
-    //     },
-    //     background: function () {
-    //         return this.$route.name + 'Colour';
-    //     }
-    // },
-    // methods: {
-    //     update: _.debounce(function (e) {
-    //         this.input = e.target.value
-    //     }, 300)
-    // }
 }).$mount('#app')
-
-
-
-
-
