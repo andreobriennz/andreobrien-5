e8ebfebc33d604cc3cc236def5e72087849e0230
andreobriennz <andre@communica.co.nz>
2018-05-15 17:20:33 +1200

    Update About text

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,15 +9,13 @@
 
         <div class="col-6 aboutText">
             <p>
-                <strong class="heading-3">About me</strong><br><br>
-
-                I am a developer with experience on dozens of web apps and websites. 
-                I am currently looking to secure a position where I can use my skills to bring immediate value and also develop my skillset further.
+                <strong class="heading-3">About me</strong>
                 <br><br>
-                As a full-stack web developer I have experience using HTML, CSS, JavaScript, PHP, NodeJS, MySQL, cloud hosting and more.
-                Of these, I consider JavaScript (including React, Vue and vanilla JavaScript) to be my strongest language, followed by PHP. 
+                Andre is a full-stack developer with a passion for learning. He has worked on a variety of websites and web apps,
+                and has commercial experience using JavaScript, CSS and PHP.
                 <br><br>
-                <!-- Some of my favourite tools include VueJS, Laravel, October CMS, Bulma, Kube, React and Express.js -->
+                He has a strong passion for JavaScript and programming,
+                and also enjoys developing with frameworks such as React, Vue and Laravel and hosting sites using cloud hosting.
             </p>
         </div>
     </section>
