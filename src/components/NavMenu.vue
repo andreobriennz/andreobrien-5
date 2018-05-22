7bbf3d22ff846ccdebf76db19a914ad4b479fb94
andreobriennz <andre@communica.co.nz>
2018-05-22 18:25:42 +1200

    Remove link to blog

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -24,7 +24,6 @@
                         <router-link to="/work" :class="{ active: page === 'work' }">Work</router-link>
                         <router-link to="/about" :class="{ active: page === 'about' }">About</router-link>
                         <router-link to="/contact" :class="{ active: page === 'contact' }">Contact</router-link>
-                        <a href="https://blog.andreobriennz.com">Blog</a>
                     </div>
                 </div>
             </div>
