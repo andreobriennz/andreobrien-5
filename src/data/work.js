5b03a2e5b6c7cbe758dad3037dd9038b228615d2
andreobriennz <andre@communica.co.nz>
2018-06-29 14:31:13 +1200

    Update work page content

--- src/data/work.js
+++ src/data/work.js
@@ -13,7 +13,7 @@ const allProjects = [
         link: 'https://andreobrien.me',
         tags: 'all vue javascript es6',
         secondaryTags: 'expressjs',
-        weight: 100
+        weight: 500
     },
     {
         title: 'Creative Monday',
@@ -41,7 +41,7 @@ const allProjects = [
         weight: 300
     },
     {
-        title: 'DH Game',
+        title: 'Game development: DH Game (vanilla JS) and 29 (React)',
         id: 'dh-game',
         description: `
             About once a year, as a personal project, I build an online game. The first time around, as part of a group project, I created the logic (JavaScript) for the game along with some of the design, CSS and backend development. The game was themed around robots vs humans, where users play cards to try and defeat the robots. 
@@ -69,19 +69,45 @@ const allProjects = [
         title: 'GitHub Projects',
         id: 'github',
         description: `
-            I have developed a few libraries and tools which I published on GitHub. 
-            Some notable mentions include
-            <a href="https://github.com/andreobriennz/Skeleton-Flexbox">a fork of the Skeleton Boilerplate which uses flexbox</a>,
-            <a href="https://github.com/andreobriennz/vario--colour-and-font-library">a CSS library of colours and fonts</a>,
-            <a href="https://github.com/andreobriennz/Vivace-2">a tool for using JavaScript and PHP together isomorphically</a> and
-            <a href="https://github.com/andreobriennz/vario--boxy-grid/blob/master/assets/js/vario.js">a mirco-library alternative to jQuery</a>
+            I have developed a few libraries and tools which I published on GitHub. Some of the most notable one are:
+            
+            <br /><br />
+
+            <h3><a href="https://github.com/andreobriennz/skeleton-flexbox">skeleton-flexbox</a></h3>
+            <p>
+                The Skeleton CSS boilerplate remains a popular boilerplate, despite the fact it hasn't been updated in the last 4 years. 
+                This means that developers are forced to develop with its outdated grid system, rather than using flexbox.
+                When I first developed skeleton-flexbox about a year ago I replaced the original grid system with my own custom flexbox grid. 
+                However, I more recently I discovered flexbox-grid by kristoferjoseph and was very impressed with it. 
+                Version 3 of skeleton-flexbox therefore switches to using his framework. 
+                It also splits the Skeleton project into seperate SCSS files and adds some additional improvements<br />
+                skeleton-flexbox is available on both <a href="https://github.com/andreobriennz/skeleton-flexbox">GitHub</a> and <a href="https://www.npmjs.com/package/skeleton-flexbox">NPM</a>. 
+                It also has its own <a href="https://andreobriennz.github.io/skeleton-flexbox/">demo site.</a>
+            </p>
+
+            <h3><a href="https://github.com/andreobriennz/kube-flexbox">kube-flexbox</a></h3>
+            <p>
+                Kube is a lesser known, but very nice CSS/SASS framework that offers a great design with less bloat than bootstrap.
+                kube-flexbox is a project very similar to skeleton-flexbox (mentioned above), 
+                but in this case updating the Kube framework to work with flexbox. 
+                <a href="https://github.com/andreobriennz/kube-flexbox">View it here</a>
+            </p>
+
+            <h3><a href="https://github.com/andreobriennz/Vivace-2">Vivace 2</a></h3>
+            <p>
+                Vivace 2 combined 2 previous tools I developed: 
+                one tool acts as a templating language allowing JavaScript and PHP together isomorphically, 
+                the other (Vario) is a JavaScript library which acts as a more lightweight alternative to jQuery.
+                I am currently developing a newer version of this tool which makes better use of ES2015 syntax.
+                <a href="https://github.com/andreobriennz/Vivace-2">a tool for using JavaScript and PHP together isomorphically</a> and
+            </p>
             `,
         image: 'work/github.jpg',
         alt: 'Showing a project from my GitHub page',
         link: 'https://github.com/andreobriennz',
         tags: 'all css javascript vanilla-js',
         secondaryTags: '',
-        weight: 900
+        weight: 10
     },
     {
         title: 'Shelter of Hope',
@@ -114,10 +140,26 @@ const allProjects = [
 
 let tagDescriptions = []
 tagDescriptions['all'] = `A collection of my favourite projects.`
-tagDescriptions['javascript'] = `JavaScript is a language I use very freqently, including React, Vue, Cypress ES6 and sometimes TypeScript. You can also view projects I have made in <a href="/work#react" class="link">React</a>, <a href="/work#vue" class="link">Vue</a> or <a href="/work#vanilla-js" class="link">Vanilla JS</a> specifically.`
-tagDescriptions['php'] = 'I have commercial experience developing with PHP. I tend to use Drupal for commerical projects and Laravel for personal projects.'
+
 tagDescriptions['sass'] = 'While nowadays I spend more time developing with JavaScript and PHP, I still enjoy developing with CSS and SASS and do so freqently. The following work shows some CSS / SASS projects I\'v worked on.'
-tagDescriptions['nodejs'] = ''
+
+tagDescriptions['javascript'] = `JavaScript is a language I use very freqently, including React, Vue, Cypress ES6 and sometimes TypeScript. You can also view projects I have made in <a href="/work#react" class="link">React</a>, <a href="/work#vue" class="link">Vue</a> or <a href="/work#vanilla-js" class="link">Vanilla JS</a> specifically.`
+tagDescriptions['react'] = `
+    I have a couple projects with React that I've developed. 
+    One of them is a card game developed with React (shown here). 
+    The other is a social media site which is currently under development, but I can show it upon request.
+    `
+tagDescriptions['vue'] = `
+    I've developed with Vue since 2016, starting with an earlier version of this portfolio which used Vue version 1.
+    This portfolio showcases my 2 most recent Vue based projects.
+    `
+tagDescriptions['vanilla-js'] = `
+    I enjoy developing with pure JavaScript, as well as with frameworks and libraries. 
+    Here is a sample of some of my projects based on pure JavaScript, as explained in the descriptions.
+`
+
+tagDescriptions['php'] = 'I have commercial experience developing with PHP. I tend to use Drupal for commerical projects and Laravel for personal projects.'
+
 tagDescriptions['tools'] = 'Over the past couple years I periodically built open-source tools. You can <a href="https://github.com/andreobriennz">view them on GitHub</a>.'
 
 export {allProjects, tagDescriptions}
